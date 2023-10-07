f0ae77a031ec8c0a7b95f83263e5db447b532461
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-07 19:51:47 +0200

    add upload.html template

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    render_template("index.html")
+def index():
+    return render_template("index.html", **os.environ)
+
+@app.route("/upload")
+def upload():
+    return
