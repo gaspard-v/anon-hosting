c3dcca298f8a13ea732a79160de528f34e9d4570
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-10 15:39:46 +0200

    add html code and css rules

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import os
 
 def get_encrypted_filepath(filename: str) -> str:
     encrypted_file_storage = os.environ.get("ENCRYPTED_FILE_STORAGE")
+    if not os.path.exists(encrypted_file_storage):
+        os.mkdir(encrypted_file_storage)
     if encrypted_file_storage:
         return os.path.join(encrypted_file_storage, filename)
     return os.path.join(filename)
