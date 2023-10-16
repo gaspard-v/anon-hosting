d73fdd21e7bf6bfea64812a47368ea07db5725cf
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-16 18:56:33 +0200

    Add download function

--- download.py
+++ download.py
@@ -1,6 +1,9 @@
 from flask import Response, request, render_template, abort
-from encryption import JWEOperation
+from encryption import JWEOperation, EncryptionOperation, UploadedFileDataStructure
 import os
+import base64
+import utils
+import mimetypes
 
 
 def handle():
@@ -9,5 +12,20 @@ def handle():
     download_key = request.form.get("download key")
     if not download_key:
         abort(400)
-    jwt = JWEOperation(download_key)
-    jwt.decode_jwt()
+    jwe = JWEOperation(download_key)
+    jwt = jwe.decode_jwt()
+    key = base64.b64decode(jwt["key"])
+    tweak = base64.b64decode(jwt["tweak"])
+    stored_filename = jwt["stored_filename"]
+    original_filename = jwt["original_filename"]
+    [content_type, _] = mimetypes.guess_type(original_filename, strict=False)
+    if not type:
+        content_type = "application/octet-stream"
+    decryptor = EncryptionOperation(key=key, tweak=tweak)
+    filepath = utils.get_encrypted_filepath(stored_filename)
+    encrypted_file = open(filepath, "rb")
+    return Response(
+        response=decryptor.stream_to_stream_decryptor(encrypted_file),
+        content_type=content_type,
+        headers={"Content-Disposition": f"attachment; filename={original_filename}"},
+    )
