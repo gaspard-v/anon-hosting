208f5cb7e13fa97e4c8a00ee71929160bf66de52
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-18 01:37:16 +0200

    Add upload csrf token

--- index.py
+++ index.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 from upload import handle as upload_handle
 from download import handle as download_handle
+from flask_wtf.csrf import CSRFProtect
 import os
 
 load_dotenv()
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
--- upload.py
+++ upload.py
@@ -3,6 +3,7 @@ import os
 from Exceptions import NoFilesUploaded
 from Cryptography.flask import UploadedFileEncryption
 from Cryptography.JWT.JWE import JWEOperation
+from flask_wtf.csrf import validate_csrf, generate_csrf, ValidationError
 
 
 def _post_file() -> dict[str, str]:
@@ -20,7 +21,9 @@ def handle():
     others = {}
     if request.method == "POST":
         try:
+            validate_csrf(request.form.get("csrf_token"))
             others["file_metadata"] = _post_file()
-        except NoFilesUploaded:
+        except (NoFilesUploaded, ValidationError):
             abort(400)
+    others["csrf_token"] = generate_csrf()
     return render_template("upload.html", **os.environ, **others)
