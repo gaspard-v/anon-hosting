1cdcc52f61c5f82263c24aa779839b9825c9e4b2
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-17 19:41:08 +0200

    creat Dataclass module

---
+++ Dataclass/__init__.py
@@ -0,0 +1,1 @@
+from .UploadedFileDataStructure import UploadedFileDataStructure
--- encryption.py
+++ encryption.py
@@ -13,25 +13,7 @@ from jwcrypto.common import json_encode
 from Exceptions import EnvVariableNotDefined
 import time
 import json
-from dataclasses import dataclass
-
-
-@dataclass
-class UploadedFileDataStructure:
-    key: str
-    tweak: str
-    original_filename: str
-    stored_filename: str
-    content_type: str
-
-    def to_dict(self) -> dict[str, str]:
-        return {
-            "key": self.key,
-            "tweak": self.tweak,
-            "original_filename": self.original_filename,
-            "stored_filename": self.stored_filename,
-            "content_type": self.content_type,
-        }
+from Dataclass import UploadedFileDataStructure
 
 
 class JWEOperation:
