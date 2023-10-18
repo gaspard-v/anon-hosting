e0b1ce4c9e6bcc5dc81cd33daf50fff87623699a
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-10-18 19:37:03 +0200

    add bytes_to_humans_readable

--- Utils/path_operation.py
+++ Utils/path_operation.py
@@ -8,3 +8,12 @@ def get_encrypted_filepath(filename: str) -> str:
     if encrypted_file_storage:
         return os.path.join(encrypted_file_storage, filename)
     return os.path.join(filename)
+
+
+def bytes_to_humans_readable(number: int) -> str:
+    unites = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+    i = 0
+    while number >= 1000 and i < len(unites) - 1:
+        number /= 1000.0
+        i += 1
+    return f"{number:.2f} {unites[i]}"
